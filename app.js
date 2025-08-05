163f82456e7c06ac69dc77d5471c56c38f6eb98d
Aman <amanfarhan2008@gmail.com>
2025-08-05 23:49:11 +0500

    ad

--- app.js
+++ app.js
@@ -1,5 +1,146 @@
-    
-    a
-    b]
-    
-    
+
+// let resultDiesEl = document.getElementById("result-dies");
+
+// let dice1 =
+//  'div class="container"<div></div><div></div><div></div><div></div><div class="special"></div><div></div><div></div><div></div><div></div></div>';
+
+// let dice2 =
+//  '<div class="container"><div class="special"></div><div></div><div></div><div></div><div></div><div class="special"></div><div></div><div></div><div></div></div>';
+
+// let dice3 =
+//  '<div class="container"><div></div><div></div><div class="special"></div><div></div><div class="special"></div><div></div><div class="special"></div><div></div><div></div></div>';
+
+
+// let dice4 =
+//  '<div class="container"><div class="special"></div><div></div><div class="special"></div><div></div><div></div><div></div><div class="special"></div><div></div><div class="special"></div></div>';
+
+// let dice5 = 
+//  '<div class="container"><div class="special"></div><div></div><div class="special"></div><div></div><div class="special"></div><div></div><div class="special"></div><div></div><div class="special"></div></div>';
+
+// let dice6 =
+//  '<div class="container"><div class="special"></div><div></div><div class="special"></div><div class="special"></div><div></div><div class="special"></div><div class="special"></div><div></div><div class="special"></div></div>';
+
+
+// function play(){
+//     let diesRoll = Math.ceil(Math.random() * 6 );
+//     if (diesRoll === 1){
+//         resultDiesEl.innerHTML = dice1;
+//     } 
+//      else if (diesRoll === 2){
+//          resultDiesEl.innerHTML = dice2;
+//     }
+//      else if (diesRoll === 3){
+//          resultDiesEl.innerHTML = dice3;
+//     }
+//     else if (diesRoll === 4){
+//          resultDiesEl.innerHTML = dice4;
+//     }
+//     else if (diesRoll === 5){
+//          resultDiesEl.innerHTML = dice5;
+//     }
+//     else if (diesRoll === 6){
+//          resultDiesEl.innerHTML = dice6;
+//     }
+
+
+// }
+
+
+
+
+//  let userName = "Talal Ahmed";
+
+//  function printName () {
+//     let userName = "Abrar Ahmed";
+//     console.log(userName);
+// }
+
+// console.log(userName);
+// printName();
+
+
+// {
+//     var a = "a";
+//     let b = "b";
+//     const c = "c";
+// }
+
+// let userName = "Talal";
+
+
+// var a = "a";
+// var a = "b";
+
+// let a = "a";
+// a = "b";
+
+// const a = "a";
+
+// console.log(a);
+
+
+
+
+// let userEmailEl = document.getElementById("email");
+// let userPasswordEl = document.getElementById("password");
+// let messageEl = document.getElementById("message");
+// let users = [];
+
+// function register () {
+//     let userInputMail = userEmailEl.value;
+//     let userInputPass = userPasswordEl.value;
+//     let user = {
+//         email: userInputMail,
+//         password: userInputPass
+//     }
+//     users.push(user);
+//     console.log(userInputMail, userInputPass);
+// }
+
+// function login () {
+//     let userInputMail = userEmailEl.value;
+//     let userInputPass = userPasswordEl.value;
+//     let message = "";
+//     for (let i = 0; i < users.length; i++) {
+//         let userObj = users[i];
+//         if (userInputMail === userObj.email && userInputPass === userObj.password) {
+//             message = "User authenticated!";
+//             break;
+//         }
+//         else if (userInputMail === userObj.email && userInputPass !== userObj.password) {
+//             message = "password incorrect!";
+//             break;
+//         }
+//         else if (userInputMail !== userObj.email || userInputPass !== userObj.password) {
+//             message = "user does not exist.";
+//         }
+//     }
+//      messageEl.innerHTML = message;
+// } 
+
+
+let userEmailEl = document.getElementById("email");
+let userPasswordEl = document.getElementById("password");
+let userMessageEl = document.getElementById("message");
+
+let users = [];
+function register (){
+     let userInputMail = userEmailEl.value;
+     let userInputPass = userEmailEl.value;
+     let user = {
+        email: userInputMail,
+        password: userInputPass
+    }
+    users.push(user);
+    console.log(userInputMail, userInputPass);
+}
+
+function login (){
+      let userInputMail = userEmailEl.value;
+     let userInputPass = userEmailEl.value;
+     let message = "";
+     for(let i = 0 ;i < users.length; i++){
+          let userObj = users[i];
+          if(userInputMail === userObj.email && userInputPass === userObj.password)
+     }
+}
